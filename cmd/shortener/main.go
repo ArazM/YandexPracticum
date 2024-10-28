19b52955a3355ff91221f68b074e455df6940fae
ArazM <adamov768@yandex.ru>
2024-10-28 17:33:42 +0300

    итерация 1.9

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -25,7 +25,7 @@ const (
 )
 
 func main() {
-	os.Setenv("CONFIG_PATH", "/Users/mamedalyevaraz/YandexPracticum/internal/config/local.yaml")
+	os.Setenv("CONFIG_PATH", "./internal/config/local.yaml")
 	cfg := config.MustLoad()
 
 	log := setupLogger(cfg.Env)
